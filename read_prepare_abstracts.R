31ce341173b662d9e01cf09e849e0ebc709dea0e
fahd09 <fhhh09@gmail.com>
2016-12-19 02:37:02 -0600

    updated analysis + visualization

--- read_prepare_abstracts.R
+++ read_prepare_abstracts.R
@@ -81,7 +81,7 @@ docs = tm_map(docs, content_transformer(removeWords), to_delete_words)          
 docs = tm_map(docs, content_transformer(stripWhitespace))     # stripe white spaces
 
 dtm <- DocumentTermMatrix(docs) # or TermDocumentMatrix
-dtm <- removeSparseTerms(dtm,0.9999) # 
+dtm <- removeSparseTerms(dtm,0.999) # 
 
 dtm.mat <- as.matrix(dtm)
 
@@ -93,9 +93,24 @@ term.freq.df <- data.frame(term=names(term.freq), freq=term.freq) # for ggplot
 ggplot(data=term.freq.df, aes(log10(freq))) + geom_histogram(bins = 30) +
   labs(title = "Term Frequency", x= "Log_10 (frequency)", y="Count")
 
+
+# we will use 4-year running interval
+
+years.list <- as.numeric(unique(format(hbr$date, '%Y')))
+years.grp <- 
+  cut(as.numeric(format(hbr$date, '%Y')), 
+    breaks=seq.int(1923, 2011, 4),
+    include.lowest=T, ordered_result = F)
+levels(years.grp) <- gsub(',', '-', substr(levels(years.grp), 2,10)) # better names
+
 # We want to know word freq. by year
+
 year_nominal <- makeNominalData(data.frame(as.factor(format(hbr$date, '%Y'))))
 colnames(year_nominal) <- substr(colnames(year_nominal), 35,40)
+
+# We want to know word freq by 'year group'
+# year_nominal <- makeNominalData(data.frame(years.grp))
+# colnames(year_nominal) <- levels(years.grp)
 
 # Now we have:
 # dtm           ==> 12697 (articles)  x 18376  (words)
@@ -108,6 +123,7 @@ word_year <- t(dtm.mat) %*% year_nominal
 
 # head(word_year)
 
+# No need for this when using the 4-year window
 yrs.sorted <- order(as.numeric(colnames(word_year)))
 word_year <- word_year[,yrs.sorted]
 #word_year[word_year==0] <- NA       # we convert all zeros to NAs 
@@ -117,7 +133,7 @@ word_year <- word_year[,yrs.sorted]
 # popular word per year
 
 pop.cnt <- apply(word_year, 2, function(x) {return( max(x) )})                                   # return maximum "count"
-pop.wrd <- as.character(apply(word_year, 2, function(x) {return( names(which(max(x)==x))[1] )})) # return word
+pop.wrd <- as.character(apply(word_year, 2, function(x) {return( names(which(x==max(x)))[1] )})) # return word
 
 pop_word_year <- data.frame(year=colnames(word_year), word=pop.wrd,count=pop.cnt)
 
@@ -136,7 +152,7 @@ res.ca <- epCA(t(word_year), graphs = F)
 # First, we give a wight to each word according to its "temporal" importance
 weight <- data.frame(word = row.names(res.ca$ExPosition.Data$fj) , 
                      wt   = as.vector(res.ca$ExPosition.Data$dj))
-top25 <- weight$wt > quantile(weight$wt, .5)
+top25 <- weight$wt > quantile(weight$wt, .75)
 
 # then visualize those "selected" ones..
 
@@ -148,15 +164,23 @@ df1<-
 df2<-
   data.frame(res.ca$ExPosition.Data$fj[top25,]) %>% 
   select(num_range("X", 1:4)) %>%
-  mutate(word= row.names(res.ca$ExPosition.Data$fj)[top25])
+  mutate(word= row.names(res.ca$ExPosition.Data$fj)[top25]) %>%
+  mutate(wt  = as.vector(res.ca$ExPosition.Data$dj)[top25])
 
 g1<-ggplot(data=df1, aes(x=X1, y=X2, label=year)) + geom_text(color="red") + geom_path()
-g2<-g1+geom_text(data=df2, aes(x=X1, y=X2, label=word), check_overlap = T, size=5)
+g2<-g1+geom_text(data=df2, aes(x=X1, y=X2, label=word, size=wt), check_overlap = T)
 g2<- g2 + labs(title="Correspondence Analysis of HBR corpus over 90 years",
-               x="Component 1", y="Component 2") + theme_void()
+               x="Component 1", y="Component 2") + theme_void() + theme(legend.position="none")
 g2
 
+clus<-hclust(dist(res.ca$ExPosition.Data$fj[top25,1:5]), method = "ward.D2")
+memb <- cutree(clus, k = 4)
 
+g1<-ggplot(data=df1, aes(x=X1, y=X2, label=year)) +  geom_text(color="blue", size=10, check_overlap = T)# + geom_path()
+g2<-g1+geom_text(data=df2, aes(x=X1, y=X2, label=word, size=wt), check_overlap = T, color=memb)
+g2<- g2 + labs(title="Term map of HBR corpus over 90 years",
+               x="Component 1", y="Component 2") + theme_void() + theme(legend.position="none")
+g2
 
-
+ggsave('./term_map.png', g2)
 
